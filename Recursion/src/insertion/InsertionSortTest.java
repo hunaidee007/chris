b2d4e083495a4835c66b6564450ffd296f8a90d4
Hunaid Husain <hunaidee007@gmail.com>
2023-10-01 10:09:51 +0100

    Insertion sort

--- Recursion/src/insertion/InsertionSortTest.java
+++ Recursion/src/insertion/InsertionSortTest.java
@@ -6,19 +6,26 @@ public class InsertionSortTest {
 
         int arr[] = {45, 5, 2, 10, 1, 4, -4, -6, 42};
         display(arr);
+        System.out.println("");
 
         // Main Loop
         for (int i = 0; i < arr.length; i++) {
+            System.out.println("----------------------");
             int valueToBeCompared = arr[i];
+            System.out.println("Value to be compared : " + valueToBeCompared);
             if (i != 0) {
                 int index = i - 1;
+                // Traverse till the first element.
                 do {
+                    System.out.println("is " + valueToBeCompared + " <  " + arr[index]);
                     if (valueToBeCompared < arr[index]) {
                         int temp = arr[index];
                         arr[index] = valueToBeCompared;
                         arr[index + 1] = temp;
+                        System.out.println("Swap these two");
                     }
                     index--;
+                    display(arr);
                 }
                 while (index >= 0);
 
@@ -35,5 +42,6 @@ public class InsertionSortTest {
         for (int i = 0; i < arr.length; i++) {
             System.out.print(arr[i] + " ");
         }
+        System.out.println("");
     }
 }
